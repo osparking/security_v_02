366a33740233a72475291ec9626d6a782d32b24d
osparking <jbpark03@gmail.com>
2024-04-03 15:04:00 +0900

    제어기 및 뷰 클래스 각 1개

---
+++ src/main/java/space/bum/spring_boot/SecurityV02Application.java
@@ -0,0 +1,13 @@
+package space.bum.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityV02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityV02Application.class, args);
+	}
+
+}
